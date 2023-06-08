2fe22d2cc8ce24a59b145711d1ea9fdb15530548
LucieChev <123562385+LucieChev@users.noreply.github.com>
2023-06-08 10:54:20 +0200

    contact form ok

--- src/components/Animalz.jsx
+++ src/components/Animalz.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import PropTypes from "prop-types";
 import { Button } from "@mui/material";
 import "./Modal.css";
@@ -10,32 +9,34 @@ import animalz4 from "../assets/Animalz4.png";
 export default function Animalz({ setShowModal }) {
   return (
     <div className="modal_page">
-      <div className="explain_project">
-        <p>
-          This is our #1 hackathon, done as part of our Wild Code School
-          training. This is a site that allows you to connect single travelers,
-          who would like to share a trip with someone who shares the same
-          desires. This project was carried out in collaboration with 3 comrades
-          for a period of 24 hours, during our 3rd month of training.
-          Constraints imposed: the theme: holidays, maximum duration: 24h. We
-          used: React, 2 APIs, a Database, LEAFLET.{" "}
-        </p>
+      <div className="modal_project">
+        <div className="explain_project">
+          <p>
+            This is our #1 hackathon, done as part of our Wild Code School
+            training. This is a site that allows you to connect single
+            travelers, who would like to share a trip with someone who shares
+            the same desires. This project was carried out in collaboration with
+            3 comrades for a period of 24 hours, during our 3rd month of
+            training. Constraints imposed: the theme: holidays, maximum
+            duration: 24h. We used: React, 2 APIs, a Database, LEAFLET.{" "}
+          </p>
+        </div>
+        <div className="project_card">
+          <img src={animalz} alt="Animalz2" className="project_image" />
+          <img src={animalz2} alt="Animalz2" className="project_image" />
+          <img src={animalz3} alt="Animalz3" className="project_image" />
+          <img src={animalz4} alt="Animalz4" className="project_image" />
+        </div>
+        <Button
+          size="small"
+          variant="contained"
+          cursor="pointer"
+          sx={{ color: "black", bgcolor: "var(--yellow)", fontFamily: "bold" }}
+          onClick={() => setShowModal(false)}
+        >
+          Close
+        </Button>
       </div>
-      <div className="project_card">
-        <img src={animalz} alt="Animalz2" className="project_image" />
-        <img src={animalz2} alt="Animalz2" className="project_image" />
-        <img src={animalz3} alt="Animalz3" className="project_image" />
-        <img src={animalz4} alt="Animalz4" className="project_image" />
-      </div>
-      <Button
-        size="small"
-        variant="contained"
-        cursor="pointer"
-        sx={{ color: "black", bgcolor: "var(--yellow)", fontFamily: "bold" }}
-        onClick={() => setShowModal(false)}
-      >
-        Close
-      </Button>
     </div>
   );
 }
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,21 +1,59 @@
-import React from "react";
+import { useRef } from "react";
 import "./Contact.css";
-import { TiTick } from "react-icons/ti";
+import emailjs from "@emailjs/browser";
 
 export default function Contact() {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_v54qs6d",
+        "template_yz6nywl",
+        form.current,
+        "2tQrjgfdb3G6W8uBL"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
     <div className="contact_page">
-      <div className="title">
-        <h3>Contact me</h3>
-        <div className="mail">
-          <TiTick />
-          to mail : lchevanche@hotmail.fr
-        </div>
-        <div className="phone">
-          <TiTick />
-          to phone : 0611418118
-        </div>
-        <div className="in_progress">(Section in progress)</div>
+      <div className="contact_title">
+        <h2>Contact me</h2>
+        <p>If you want to contact me, you can send me an email.</p>
+      </div>
+      <div className="form">
+        <form ref={form} onSubmit={sendEmail}>
+          <div className="center">
+            <label>Name</label>
+          </div>
+          <div className="center">
+            <input type="text" name="name" className="input" />
+          </div>
+          <div className="center">
+            <label>Email</label>
+          </div>
+          <div className="center">
+            <input type="email" name="email" className="input" />
+          </div>
+          <div className="center">
+            <label>Message</label>
+          </div>
+          <div className="center">
+            <textarea name="message" className="textarea" />
+          </div>
+          <div className="center">
+            <input type="submit" value="Send" className="send_button" />
+          </div>
+        </form>
       </div>
     </div>
   );
