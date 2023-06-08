0434492dc595ff5b56d65004c4b49851afc5fb11
LucieChev <123562385+LucieChev@users.noreply.github.com>
2023-06-08 14:02:04 +0200

    home page done

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import githubLogo from "../assets/logo/github.png";
 import linkedinLogo from "../assets/logo/th.jpg";
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
-import React from "react";
 import "./Home.css";
 import dev from "../assets/dev.jpg";
+import { NavLink } from "react-router-dom";
 
 function Home() {
   return (
@@ -14,7 +14,7 @@ function Home() {
       }}
     >
       <div className="home_title">
-        <h1>Hi, I'm Lucie,</h1>
+        <h1>Hi, I am Lucie,</h1>
       </div>
       <div className="sub_title">
         <h2>Web developer</h2>
@@ -22,6 +22,13 @@ function Home() {
       <div className="sub_title">
         <h3>Front-End / JS / REACT </h3>
         <h3>Full-Stack</h3>
+      </div>
+      <div>
+        <button type="button" className="button_home_page">
+          <NavLink to="/contact" className="contact_button_home_page">
+            CONTACT ME
+          </NavLink>
+        </button>
       </div>
     </div>
   );
